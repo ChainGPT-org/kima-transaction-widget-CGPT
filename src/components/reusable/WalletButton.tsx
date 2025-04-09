e8004b40bea4736097d17775f84340077a5ac98c
Sajjad Akhtar <sajjad.akhtar@funavry.com>
2025-04-09 11:25:26 +0500

    Chaingpt Main

--- src/components/reusable/WalletButton.tsx
+++ src/components/reusable/WalletButton.tsx
@@ -102,7 +102,7 @@ const WalletButton = ({ errorBelow = false }: { errorBelow?: boolean }) => {
     console.info('Handling click: Case EVM', 4)
     try {
       console.info('Attempting to open AppKitModal')
-      // await open() // Ensure await usage
+      await open() // Ensure await usage
       console.info('AppKitModal opened successfully')
     } catch (error) {
       console.error('Failed to open AppKitModal', error)
@@ -135,18 +135,17 @@ const WalletButton = ({ errorBelow = false }: { errorBelow?: boolean }) => {
       data-testid='connect-wallet-btn'
     >
       <div className='info-wrapper'>
-        {isReady && (
-          <button
-            className={`hex-button ${isReady ? 'connected' : 'disconnected'} ${width < 640 && 'shortened'} ${theme.colorMode}`}
-            onClick={handleClick}
-          >
+        <button
+          className={`hex-button ${isReady ? 'connected' : 'disconnected'} ${width < 640 && 'shortened'} ${theme.colorMode}`}
+          onClick={handleClick}
+        >
           {isReady
             ? width >= 640
               ? `${walletAddress || ''}`
               : getShortenedAddress(walletAddress || '')
             : ''}
           {!isReady && 'CONNECT WALLET'}
-        </button>)}
+        </button>
 
         {isReady && <CopyButton text={walletAddress as string} />}
       </div>
