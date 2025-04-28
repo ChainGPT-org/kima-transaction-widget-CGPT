96c7df6b3b6b7c38e342b458e213aad15e6eb4ab
Sajjad Akhtar <sajjad.akhtar@funavry.com>
2025-04-28 12:24:46 +0500

    Fixed Max Amount

--- src/components/reusable/SingleForm.tsx
+++ src/components/reusable/SingleForm.tsx
@@ -182,8 +182,9 @@ const SingleForm = ({
             <SecondaryButton
               className='max-button'
               clickHandler={() => {
-                setAmountValue(maxValue.toString())
-                dispatch(setAmount(maxValue.toString()))
+                const formattedMaxValue = maxValue.toFixed(2);
+                setAmountValue(formattedMaxValue);
+                dispatch(setAmount(formattedMaxValue));
               }}
             >
               MAX
