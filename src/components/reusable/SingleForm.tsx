1099e35c79f245aa8eebd25b76fc16f4ec93d2b1
Sajjad Akhtar <sajjad.akhtar@funavry.com>
2025-04-30 12:52:40 +0500

    Fixed Decimals Issue

--- src/components/reusable/SingleForm.tsx
+++ src/components/reusable/SingleForm.tsx
@@ -172,9 +172,7 @@ const SingleForm = ({
               const maskedValue = value
                 .replace(/[^0-9.]/g, '') // Remove non-numeric and non-dot characters
                 .replace(/(\..*?)\..*/g, '$1') // Allow only one dot
-                // .replace(new RegExp(`(\\.\\d{${decimals}})\\d+`), '$1') // Limit decimal places
-                .replace(/^0+(?=\d)/, '') // Remove leading zeros unless followed by a dot
-                .replace(/(\.\d{2})\d+/, '$1') // Limit decimal places to 2
+                .replace(new RegExp(`(\\.\\d{${decimals}})\\d+`), '$1') // Limit decimal places
 
               setAmountValue(maskedValue)
               dispatch(setAmount(maskedValue))
